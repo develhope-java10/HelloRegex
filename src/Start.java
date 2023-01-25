519a392a385aa5a4509ba41c9142da21736054c1
lulanoise <lucrezia.rumore98@gmail.com>
2023-01-25 16:45:57 +0100

    add parsenames

--- src/Start.java
+++ src/Start.java
@@ -1,15 +1,11 @@
 import javax.swing.text.html.HTMLDocument;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 public class Start {
     public static void main(String[] args){
         String names = "Giulia 1 \n Giada 2 \n Lucrezia 3 \n Tommaso 4 \n Federico 5";
-        Pattern pattern = Pattern.compile("[a-zA-Z]+");
-        Matcher matcher = pattern.matcher(names);
-        while(matcher.find()){
-            String print = names.substring(matcher.start(), matcher.end());
-            System.out.println(print);
-        }
+        parseNames(names);
 
         ICollege[] colleges = new ICollege[]{
                 new MiddleCollege(),
@@ -34,4 +30,15 @@ public class Start {
             }
         }
     }
-}
+
+    public static String[] parseNames (String names) {
+        ArrayList<String> nameList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
+        Matcher matcher = pattern.matcher(names);
+        while (matcher.find()) {
+            String name = names.substring(matcher.start(), matcher.end());
+            nameList.add(name);
+        }
+        return nameList.toArray(new String[0]);
+    }
+    }
